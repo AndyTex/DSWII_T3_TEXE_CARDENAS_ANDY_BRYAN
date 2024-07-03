62d6ec696b4405e8129a7998cf1812855156d297
WhiteNight07181 <andycnblue1234@gmail.com>
2024-07-02 22:18:17 -0500

    pregunta 1 commit 1

---
+++ src/main/java/pe/edu/cibertec/DSWII_T3_TexeAndy/DswiiT3TexeAndyApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_TexeAndy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3TexeAndyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3TexeAndyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWII_T3_TexeAndy/DswiiT3TexeAndyApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_T3_TexeAndy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3TexeAndyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
